e6e0016845bf981c27cf688df8ceab0be3973aaa
cbnuLeehyunwoo <henow123@naver.com>
2026-03-28 20:05:24 +0900

    fix: App.kt 파일에서 프로덕션 코드를 호출하지 않던 문제 수정
    - ProjectScreen()을 호출하고, AppPreview 컴포저블 함수가 @Preview 역할을 수행하도록 수정하였습니다.

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/App.kt
@@ -4,9 +4,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Devices.DESKTOP
 import androidx.compose.ui.tooling.preview.Preview
+import woowacourse.kanban.ui.project.ProjectScreen
+
+@Composable
+fun App() {
+    MaterialTheme {
+        ProjectScreen()
+    }
+}
 
 @Composable
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF, device = DESKTOP)
-fun App() {
-    MaterialTheme {}
+private fun AppPreview() {
+    ProjectScreen()
 }
