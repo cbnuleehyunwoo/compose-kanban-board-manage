748a5cd82530ac5d6c0c6ee57510924ecbb1ac50
cbnuLeehyunwoo <henow123@naver.com>
2026-03-29 14:47:59 +0900

    refactor: 테스트 명이 테스트 코드를 좀 더 잘 나타내도록 수정 및 불필요한 공백 제거

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/domain/project/ProjectTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/domain/project/ProjectTest.kt
@@ -1,7 +1,6 @@
 package woowacourse.kanban.domain.project
 
 import androidx.compose.ui.test.ExperimentalTestApi
-import jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle
 import org.assertj.core.api.Assertions.assertThat
 import woowacourse.kanban.domain.board.Board
 import woowacourse.kanban.domain.card.Card
@@ -13,7 +12,7 @@ import kotlin.test.Test
 class ProjectTest {
 
     @Test
-    fun `프로젝트가 1개 이상의 보드를 가진다`() {
+    fun `프로젝트는 1개 이상의 보드를 가진다`() {
         val project = Project(
             projectTitle = "제목",
             projectDescription = "내용",
@@ -21,20 +20,15 @@ class ProjectTest {
         assertThat(project.boards.size).isEqualTo(1)
     }
 
-
-
-
-
-
     @Test
-    fun `프로젝트가 1개 이상의 보드를 가진다1`() {
+    fun `초기 프로젝트 생성 시 보드를 추가할 수 있다`() {
         val boardTitle: String = "abcd"
         val board: Board = Board(boardTitle = boardTitle)
         val project: Project = Project(
             boardList = listOf(board),
             selectedBoardIndex = 0,
             projectTitle = "프로젝트 제목",
-            projectDescription = "프로젝트 설명"
+            projectDescription = "프로젝트 설명",
         )
 
         assertThat(project.selectedBoard.title).isEqualTo(boardTitle)
@@ -50,7 +44,7 @@ class ProjectTest {
             boardList = listOf(oldBoard, newBoard),
             selectedBoardIndex = 0,
             projectTitle = "프로젝트 제목",
-            projectDescription = "프로젝트 설명"
+            projectDescription = "프로젝트 설명",
         )
 
         project = project.switchBoard(1)
