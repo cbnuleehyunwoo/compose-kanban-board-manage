df48356d72ebc76f81328a2aa20855ec66968cef
cbnuLeehyunwoo <henow123@naver.com>
2026-04-04 00:47:56 +0900

    test: 태스크 수정 시 스낵바가 표시된다

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/ui/project/ProjectScreenTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/ui/project/ProjectScreenTest.kt
@@ -1,5 +1,9 @@
 package woowacourse.kanban.ui.project
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
@@ -7,7 +11,14 @@ import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.runComposeUiTest
 import woowacourse.kanban.domain.board.Board
+import woowacourse.kanban.domain.board.BoardStateHolder
+import woowacourse.kanban.domain.card.Card
+import woowacourse.kanban.domain.card.CardManagerStatus
+import woowacourse.kanban.domain.card.CardTaskStatus
+import woowacourse.kanban.domain.dialog.DialogStateHolder
+import woowacourse.kanban.domain.dialog.EditDialogStateHolder
 import woowacourse.kanban.domain.project.Project
+import woowacourse.kanban.ui.board.BoardScreen
 import kotlin.test.Test
 
 @OptIn(ExperimentalTestApi::class)
@@ -54,4 +65,36 @@ class ProjectScreenTest {
 
         onNodeWithText("새로운 태스크가 추가되었습니다.").assertExists()
     }
+
+
+    @Test
+    fun `태스크 수정 시 스낵바가 표시된다`() = runComposeUiTest {
+        // given
+        val initialBoard = Board(
+            boardTitle = "Compose Desktop 칸반 보드",
+            cardList = listOf(Card.create(
+                title = "수정 테스트",
+                content = "",
+                tags = listOf(),
+                manager = CardManagerStatus.DINO,
+                state = CardTaskStatus.TODO,
+            )),
+        )
+        setContent {
+            ProjectScreen(
+                initialProject = Project(
+                    boardList = listOf(
+                        initialBoard,
+                    ),
+                ),
+            )
+        }
+        // when & then
+        onNodeWithTag("카드_${initialBoard.cardList.first().id}").performClick()
+        onNodeWithTag("titleTextField").performTextInput("수정된 제목")
+        onNodeWithText("수정").performClick()
+        onNodeWithText("수정된 제목").assertDoesNotExist()
+        onNodeWithText("태스크가 수정되었습니다.").assertExists()
+    }
+
 }
